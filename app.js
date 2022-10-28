e9871891f6300a4231ac3ece2520173e6911bd35
Zuhair <zuhairahmed055@gmail.com>
2022-10-28 09:20:14 +0530

    Level 2 security with basic encryption using mongoose-encrypt package and dotenv to store sensitive data like APIs and key values.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();//stores environment variable
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");//encrypt the data according to the secret key value
 
 const app = express();
 
@@ -15,6 +17,8 @@ const dataSchema = new mongoose.Schema({
     email : String,
     password : String
 });
+
+dataSchema.plugin(encrypt,{secret : process.env.SECRET , encryptedFields : ["password"]});
 
 const User = new mongoose.model("User",dataSchema);
 
