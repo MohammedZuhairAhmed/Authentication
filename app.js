936db95cedd45e518fd2519c172c1958b603fb62
Zuhair <zuhairahmed055@gmail.com>
2022-10-28 09:40:32 +0530

    Level 4 security with salting and hashing using bcrypt package

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");//to encrypt using hashing
+const bcrypt = require("bcrypt");//more advanced than md5
+const saltRounds = 10;//for salting the generated hashes
 
 const app = express();
 
@@ -34,42 +35,48 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-    const email = req.body.username;
-    const pass = md5(req.body.password);
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
 
-    const user = new User({
-        email : email,
-        password : pass
-    });
+        const email = req.body.username;
+        const pass = hash;
+
+        const user = new User({
+            email : email,
+            password : pass
+        });
 
     user.save(function(err){
-        if(!err)
-            res.render("secrets");
-        else
+        if(err)
             console.log(err);
+        else
+            res.render("secrets");
+
+        });    
+    
     });
 
 });
 
 app.post("/login",function(req,res){
-
     const email = req.body.username;
-    const pass = md5(req.body.password);
+    const pass = req.body.password;
 
     User.findOne({email : email},function(err,foundUser){
-
-        if(!err)
+        if(err)
+            console.log(err);
+        else
         {
             if(foundUser)
             {
-                if(foundUser.password === pass )
-                    res.render("secrets");
+                bcrypt.compare(pass, foundUser.password, function(err, result) {
+                    if(result === true)
+                        res.render("secrets");
+                    else
+                        console.log(err);    
+                });
             }
-        }else
-            console.log(err);
-
+        }    
     });
-
 });
 
 app.listen("3000",function(req,res){
