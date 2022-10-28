3ef1263d354a3c71b4dd4474346e1ccb14efa1b1
Zuhair <zuhairahmed055@gmail.com>
2022-10-29 00:31:39 +0530

    Final completed project with level 6 oAuth authentication and access for users to add their secrets to page

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ mongoose.connect("mongodb://0.0.0.0:27017/usersDB");
 const dataSchema = new mongoose.Schema({
     email : String,
     password : String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 dataSchema.plugin(passportLocalMongoose);
@@ -95,10 +96,40 @@ app.get("/register",function(req,res){
 
 app.get("/secrets",function(req,res){
 
+    User.find({"secret" : {$ne : null}} , function(err,foundUsers){
+        if(err)
+            console.log(err);
+        else
+        {
+            if(foundUsers)
+                res.render("secrets",{usersWithSecrets : foundUsers });
+        }    
+    })
+
+});
+app.get("/submit",function(req,res){
+    
     if(req.isAuthenticated()){
-        res.render("secrets");   
+        res.render("submit");   
     }else
         res.redirect("/login");
+});
+
+app.post("/submit",function(req,res){
+    const secr = req.body.secret;
+
+    User.findById(req.user.id,function(err,foundUser){
+        if(err)
+            console.log(err);
+        else
+        {
+            if(foundUser)
+                foundUser.secret = secr;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+        }    
+    });
 });
 
 app.get("/logout",function(req,res){
